b381b9a2a77038c991718a006d8f9e2254c579bc
Richard Styron <richard.h.styron@gmail.com>
2017-05-30 16:14:11 +0200

    more mexican metadata

--- scripts/utils/mexico.py
+++ scripts/utils/mexico.py
@@ -12,6 +12,7 @@ def process_mexico(mx_df):
     mx_df['average_dip'] = mx_df.apply(make_dip_tup, axis=1)
     mx_df['net_slip_rate'] = mx_df.apply(process_slip_rate, axis=1)
     mx_df['slip_type'] = mx_df.apply(process_fault_type, axis=1)
+    mx_df['epistemic_quality'] = mx_df.apply(process_clase, axis=1)
 
     return mx_df
 
@@ -50,13 +51,17 @@ def process_fault_type(row):
 
 def make_dip_tup(row):
 
-    return '({},,)'.format(row['Inclin'])
+    if (row['Inclin']) is not None:
+        dip_tup = '({},,)'.format(row['Inclin'])
+    else:
+        dip_tup = None
+    return dip_tup
 
 
 def process_slip_rate(row):
     if (row['Taza_des'] is not None) and ~np.isnan(row['Taza_des']):
         sr = '({},,)'.format(row['Taza_des'])
-    elif (row['Tasa_Des'] is not None) and ~np.isnan(row['Taza_des']):
+    elif (row['Tasa_Des'] is not None) and ~np.isnan(row['Tasa_Des']):
         sr = '({},,)'.format(row['Tasa_Des'])
     else:
         sr = None
@@ -64,3 +69,10 @@ def process_slip_rate(row):
     return sr
 
 
+def process_clase(row):
+    if row['clase'] == 'A':
+        return 1
+    elif row['clase'] == 'B':
+        return 2
+    elif row['clase'] == 'C':
+        return 3
