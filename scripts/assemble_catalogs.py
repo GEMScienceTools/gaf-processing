e2ec3a809edf040682d9b8bab26bf5eb4049eb96
Richard Styron <richard.h.styron@gmail.com>
2017-10-26 12:03:04 +0200

    changed header_match file from json to ini

--- scripts/assemble_catalogs.py
+++ scripts/assemble_catalogs.py
@@ -1,7 +1,6 @@
 import shutil
 import configparser
 import json
-#import sqlalchemy as sqa
 import numpy as np
 import pandas as pd
 import geopandas as gpd
@@ -11,11 +10,10 @@ from utils import catalog_conversions as cc
 # import empty DF for the master database
 master_df = pd.read_csv('../master_df_schema.csv', index_col=1)
 
-## import dictionary to match catalog columns
-cat_header_match_file = 'regional_catalog_header_matching.json'
-
-with open(cat_header_match_file, 'r') as f:
-    header_dict = json.load(f)
+## import ini file to match catalog columns
+cat_header_match_file = 'regional_catalog_header_matching.ini'
+header_cfg = configparser.ConfigParser()
+header_cfg.read(cat_header_match_file)
 
 # load catalog processing configuration
 cfg_file = './catalog_process_config.ini'
@@ -28,13 +26,13 @@ catalog_list = cc.get_catalogs_from_config(cfg)
 for catalog in catalog_list:
     if catalog in cfg.sections():
         print('processing {}'.format(catalog))
-        master_df = cc.process_catalog(catalog, cfg, header_dict, master_df) 
+        master_df = cc.process_catalog(catalog, cfg, header_cfg, master_df) 
 
 master_df = gpd.GeoDataFrame(master_df) # convert to GeoPandas GeoDataFrame
 
 print('writing output files')
 
-master_df.to_file('../outputs/geojson/gem_active_faults.geojson', 
+master_df.to_file('../outputs/geojson/gem_active_faults.geojson',
                   driver="GeoJSON")
 master_df.to_file('../outputs/shapefile/gem_active_faults.shp',
                   driver="ESRI Shapefile")
