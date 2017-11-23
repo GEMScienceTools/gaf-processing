19f4b562899caff30ecbeb2b8e05e3a11187b7c6
Richard Styron <richard.h.styron@gmail.com>
2017-11-23 14:28:19 +0100

    filtering algorithms for most/all datasets

--- scripts/harmonize_catalogs.py
+++ scripts/harmonize_catalogs.py
@@ -2,104 +2,91 @@ import os
 import geopandas as gpd
 
 
-from utils.filtering import *
+from utils.filtering import (drop_bad_geometries,
+                             filter_faults_inside_other_dataset,
+                             filter_faults_crossing_other_faults)
+
 
 master_df = gpd.read_file('../outputs/geojson/gem_active_faults.geojson')
 mdf_write_path = '../outputs/geojson/gem_active_faults_harmonized.geojson'
 
-
 mdf = master_df.copy(deep=True)
+n_faults_init = len(mdf)
 
-def drop_bad_geometries(master_df, verbose=False):
-    n_faults = master_df.shape[0]
-
-    bad_geoms = [f.geometry is None for i, f in master_df.iterrows()]
-    bad_geom_inds = master_df.loc[bad_geoms].index
-
-    mdf = master_df.drop(bad_geom_inds)
-    new_n_faults = mdf.shape[0]
-
-    if verbose is True:
-        print('Removed {} faults with bad geometries'.format(
-                                                        n_faults-new_n_faults))
-    return mdf
-
-
-def filter_share_emme_overlap(master_df, verbose=False):
-    """
-    Filters out faults from the SHARE catalog where they fall within
-    the polygon defined by the newer EMME catalog.
-    """
-    emme_faults = master_df[master_df.catalog_name == 'EMME']
-    share_faults = master_df[master_df.catalog_name == 'SHARE']
-
-    share_in_emme_idx = get_fault_inds_inside_other_dataset(share_faults,
-                                                            emme_faults)
-    mdf = master_df.drop(share_in_emme_idx)
-
-    return mdf
-
-
-def filter_thailand_crossing_myanmar(master_df, verbose=False):
-
-    thai_faults = master_df[master_df.catalog_name == 'thailand']
-    myan_faults = master_df[master_df.catalog_name == 'myanmar']
-
-    thai_myan_crossings = fault_intersection_inds(thai_faults, myan_faults)
-
-    if verbose is True:
-        print(thai_myan_crossings)
-
-    mdf = master_df.drop(thai_myan_crossings)
-
-    return mdf
-
-
-def filter_himatibetmap_crossing_myanmar(master_df, verbose=False):
-
-    hima_faults = master_df[master_df.catalog_name == 'HimaTibetMap']
-    myan_faults = master_df[master_df.catalog_name == 'myanmar']
-
-    hima_myan_crossings = fault_intersection_inds(hima_faults, myan_faults)
-
-    if verbose is True:
-        print(hima_myan_crossings)
-
-    mdf = master_df.drop(hima_myan_crossings)
-
-    return mdf
-
-
-def filter_thailand_crossing_himatibetmap(master_df, verbose=False):
-
-    thai_faults = master_df[master_df.catalog_name == 'thailand']
-    hima_faults = master_df[master_df.catalog_name == 'HimaTibetMap']
-
-    thai_hima_crossings = fault_intersection_inds(thai_faults, hima_faults)
-
-    if verbose is True:
-        print(thai_hima_crossings)
-
-    mdf = master_df.drop(thai_hima_crossings)
-
-    return mdf
-
-
-print('Dropping faults with bad geometries')
+print('Dropping faults with bad geometries...')
 mdf = drop_bad_geometries(mdf, verbose=True)
 
 print('Filtering SHARE faults with EMME overlap...')
-mdf = filter_share_emme_overlap(mdf)
+mdf = filter_faults_inside_other_dataset(mdf, 'SHARE', 'EMME')
+
+print('Filtering SHARE faults with N_Africa overlap...')
+mdf = filter_faults_inside_other_dataset(mdf, 'SHARE', 'GEM_N_Africa',
+                                         inside_type='intersects')
 
 print('Filtering Thailand faults that cross Myanmar faults...')
-mdf = filter_thailand_crossing_myanmar(mdf)
+mdf = filter_faults_crossing_other_faults(mdf, 'thailand', 'myanmar')
 
 print('Filtering HimatTibetMap faults that cross Myanmar faults...')
-mdf = filter_himatibetmap_crossing_myanmar(mdf)
+mdf = filter_faults_crossing_other_faults(mdf, 'HimaTibetMap', 'myanmar')
+
+print('Filtering HimatTibetMap faults that cross EMME faults...')
+mdf = filter_faults_crossing_other_faults(mdf, 'HimaTibetMap', 'EMME')
+
+print('Filtering HimatTibetMap faults that cross NE Asia faults...')
+mdf = filter_faults_crossing_other_faults(mdf, 'HimaTibetMap', 'GEM_NE_Asia')
 
 print('Filtering Thailand faults that cross HimatTibetMap faults...')
-mdf = filter_thailand_crossing_himatibetmap(mdf)
+mdf = filter_faults_crossing_other_faults(mdf, 'thailand', 'HimaTibetMap')
 
+print('Filtering Bird faults that cross SHARE faults...')
+mdf = filter_faults_crossing_other_faults(mdf, 'Bird 2003', 'SHARE')
+
+print('Filtering Bird faults with CCARA overlap...')
+mdf = filter_faults_inside_other_dataset(mdf, 'Bird 2003',
+                                         'GEM_Central_Am_Carib',
+                                         inside_type='intersects')
+
+#print('Filtering Bird faults with ATA overlap...')
+#mdf = filter_faults_inside_other_dataset(mdf, 'Bird 2003',
+#                                         'Active Tectonics of the Andes',
+#                                         inside_type='intersects')
+
+print('Filtering Bird faults that cross ATA faults...')
+mdf = filter_faults_crossing_other_faults(mdf, 'Bird 2003',
+                                          'Active Tectonics of the Andes')
+
+print('Filtering Bird faults with SARA overlap...')
+mdf = filter_faults_inside_other_dataset(mdf, 'Bird 2003', 'SARA',
+                                         inside_type='intersects')
+
+print('Filtering Bird faults with EMME overlap...')
+mdf = filter_faults_inside_other_dataset(mdf, 'Bird 2003', 'EMME',
+                                         inside_type='intersects')
+
+print('Filtering ATA faults that cross SARA faults...')
+mdf = filter_faults_crossing_other_faults(mdf, 'Active Tectonics of the Andes',
+                                          'SARA')
+
+print('Filtering Mexico faults that cross US Faults...')
+mdf = filter_faults_crossing_other_faults(mdf, 'Villegas Mexico',
+                                          'USGS Hazfaults 2014')
+
+print('Filtering Africa faults with EMME overlap...')
+mdf = filter_faults_inside_other_dataset(mdf, 'Macgregor_AfricaFaults', 'EMME',
+                                         inside_type='intersects')
+
+print('Filtering Africa faults with SHARE overlap...')
+mdf = filter_faults_inside_other_dataset(mdf, 'Macgregor_AfricaFaults', 
+                                         'SHARE',
+                                         inside_type='intersects')
+
+
+n_faults_final = len(mdf)
+n_faults_removed = n_faults_init - n_faults_final
+
+print('Done filtering.\n{} initial faults.\n'.format(n_faults_init) +
+      'Removed {} faults.\n{} final faults.'.format(n_faults_removed,
+                                                    n_faults_final))
 
 try:
     os.remove(mdf_write_path)
